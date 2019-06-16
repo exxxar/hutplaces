df367a86d746f909dc6e1193535bd45501119e9e
Alexandr Likholetov <likholetovalex@gmail.com>
2019-06-16 22:04:50 +0300

    add fillable

--- app/Achievements.php
+++ app/Achievements.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Achievements extends Model
 {
-    //
+    protected $fillable = [
+        'category',
+        'type',
+        'title',
+        'description',
+        'discount',
+        'exp',
+        'coins',
+        'money',
+        'items_id',
+        'cards_id',
+        'images_storage_id',
+        'isActive',
+    ];
 }
--- app/Promocodes.php
+++ app/Promocodes.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Promocodes extends Model
 {
-    //
+    protected $fillable = [
+        'lifetime',
+        'isActive',
+        'title',
+        'description',
+        'activation_count',
+        'count',
+        'money',
+        'exp',
+        'discount',
+        'items_id',
+        'cards_id',
+        'images_storage_id',
+    ];
 }
