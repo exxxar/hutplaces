417bac032f20795b60abda91c4429e77129649eb
Alexandr Likholetov <likholetovalex@gmail.com>
2019-08-29 12:45:42 +0300

    auction views

--- resources/views/admin/auction/create.blade.php
+++ resources/views/admin/auction/create.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Create New Setting</h2>
+                <h2>Create New Auction</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('auction.index') }}"> Back</a>
             </div>
         </div>
     </div>
@@ -16,6 +16,7 @@
 
     @if (count($errors) > 0)
         <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert">×</button>
             <strong>Whoops!</strong> There were some problems with your input.<br><br>
             <ul>
                 @foreach ($errors->all() as $error)
@@ -26,25 +27,84 @@
     @endif
 
 
-    {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
+    {!! Form::open(array('route' => 'auction.store','method'=>'POST')) !!}
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+            <div class="col-sm-12">
+
+                <div class="form-group">
+                    <label for="title">title</label>
+                    <input class="form-control" type="text" placeholder="title" name="title">
+                </div>
+
+                <div class="form-group">
+                    <label for="console_type">console_type</label>
+                    <select class="form-control" name="console_type">
+                        <option value=0>xbox</option>
+                        <option value=1>ps4</option>
+                        <option value=2>pc</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="lot_type">lot_type</label>
+                    <select class="form-control" name="lot_type">
+                        <option value=0>card</option>
+                        <option value=1>coins</option>
+                        <option value=2>other</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="game_type">game_type</label>
+                    <select class="form-control" name="game_type">
+                        <option value=0>NHL</option>
+                        <option value=1>NBA</option>
+                        <option value=2>FIFA</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="lifetime">lifetime</label>
+                    <select class="form-control" name="lifetime">
+                        <option value=0>unlimited</option>
+                        <option value=1>12</option>
+                        <option value=2>24</option>
+                        <option value=3>36</option>
+                        <option value=4>48</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="step_price">step_price</label>
+                    <input class="form-control" type="number" min="0" placeholder="step_price" name="step_price">
+                </div>
+
+                <div class="form-group">
+                    <label for="bid_price">bid_price</label>
+                    <input class="form-control" type="number" min="0" placeholder="bid_price" name="bid_price">
+                </div>
+
+                <div class="form-group">
+                    <label for="lot_id">lot_id</label>
+                    <input class="form-control" type="number" placeholder="lot_id" name="lot_id" value="1">
+                </div>
+
+                <div class="form-group">
+                    <label for="buy_price">buy_price</label>
+                    <input class="form-control" type="number" min="0" placeholder="buy_price" name="buy_price">
+                </div>
+
+                <div class="form-group">
+                    <label for="active">active</label>
+                    <select class="form-control" name="is_active">
+                        <option value=1>yes</option>
+                        <option value=0>no</option>
+                    </select>
+                </div>
+
+
+
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Permission:</strong>
-                <br/>
-                @foreach($permission as $value)
-                    <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
-                        {{ $value->name }}</label>
-                    <br/>
-                @endforeach
-            </div>
-        </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
--- resources/views/admin/auction/edit.blade.php
+++ resources/views/admin/auction/edit.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Edit Role</h2>
+                <h2>Edit Auction</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('auction.index') }}"> Back</a>
             </div>
         </div>
     </div>
@@ -16,6 +16,7 @@
 
     @if (count($errors) > 0)
         <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert">×</button>
             <strong>Whoops!</strong> There were some problems with your input.<br><br>
             <ul>
                 @foreach ($errors->all() as $error)
@@ -26,28 +27,170 @@
     @endif
 
 
-    {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
+    {!! Form::model($auction, ['method' => 'PATCH','route' => ['auction.update', $auction->id]]) !!}
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Permission:</strong>
-                <br/>
-                @foreach($permission as $value)
-                    <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
-                        {{ $value->name }}</label>
-                    <br/>
-                @endforeach
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+            <div class="col-sm-12">
+
+                    <div class="form-group">
+                        <label for="title">title</label>
+                        <input class="form-control" type="text" placeholder="title" name="title" value={{$auction->title}}>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="console_type">console_type</label>
+                        <select class="form-control" name="console_type">
+                            @switch($auction->console_type)
+                                @case(0)
+                                    <option value=0>xbox</option>
+                                    <option value=1>ps4</option>
+                                    <option value=2>pc</option>
+                                    @break
+
+                                @case(1)
+                                    <option value=1>ps4</option>
+                                    <option value=0>xbox</option>
+                                    <option value=2>pc</option>
+                                    @break
+
+                                @case(2)
+                                    <option value=2>pc</option>
+                                    <option value=1>ps4</option>
+                                    <option value=0>xbox</option>
+                                    @break
+                            @endswitch
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="lot_type">lot_type</label>
+                        <select class="form-control" name="lot_type">
+                            @switch($auction->lot_type)
+                                @case(0)
+                                    <option value=0>card</option>
+                                    <option value=1>coins</option>
+                                    <option value=2>other</option>
+                                    @break
+
+                                @case(1)
+                                    <option value=1>coins</option>
+                                    <option value=0>card</option>
+                                    <option value=2>other</option>
+                                    @break
+
+                                @case(2)
+                                    <option value=2>other</option>
+                                    <option value=0>card</option>
+                                    <option value=1>coins</option>
+                                    @break
+                            @endswitch
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="game_type">game_type</label>
+                        <select class="form-control" name="game_type">
+                            @switch($auction->game_type)
+                                @case(0)
+                                    <option value=0>NHL</option>
+                                    <option value=1>NBA</option>
+                                    <option value=2>FIFA</option>
+                                    @break
+
+                                @case(1)
+                                    <option value=1>NBA</option>
+                                    <option value=0>NHL</option>
+                                    <option value=2>FIFA</option>
+                                    @break
+
+                                @case(2)
+                                    <option value=2>FIFA</option>
+                                    <option value=0>NHL</option>
+                                    <option value=1>NBA</option>
+                                    @break
+                            @endswitch
+
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="step_price">step_price</label>
+                        <input class="form-control" type="number" min="0" placeholder="step_price" name="step_price" value={{$auction->step_price}}>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="bid_price">bid_price</label>
+                        <input class="form-control" type="number" min="0" placeholder="bid_price" name="bid_price" value={{$auction->bid_price}}>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="buy_price">buy_price</label>
+                        <input class="form-control" type="number" min="0" placeholder="buy_price" name="buy_price" value={{$auction->buy_price}}>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="active">active</label>
+                        <select class="form-control" name="is_active">
+                            @if($auction->active == 1)
+                                <option value=1>yes</option>
+                                <option value=0>no</option>
+                            @else
+                                <option value=0>no</option>
+                                <option value=1>yes</option>
+                            @endif
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="lifetime">lifetime</label>
+                        <select class="form-control" name="lifetime">
+                            @switch($auction->lifetime)
+                                @case(0)
+                                    <option value=0>unlimited</option>
+                                    <option value=1>12</option>
+                                    <option value=2>24</option>
+                                    <option value=3>36</option>
+                                    <option value=4>48</option>
+                                    @break
+
+                                @case(1)
+                                    <option value=1>12</option>
+                                    <option value=0>unlimited</option>
+                                    <option value=2>24</option>
+                                    <option value=3>36</option>
+                                    <option value=4>48</option>
+                                    @break
+
+                                @case(2)
+                                    <option value=2>24</option>
+                                    <option value=0>unlimited</option>
+                                    <option value=1>12</option>
+                                    <option value=3>36</option>
+                                    <option value=4>48</option>
+                                    @break
+
+                                @case(3)
+                                    <option value=3>36</option>
+                                    <option value=0>unlimited</option>
+                                    <option value=1>12</option>
+                                    <option value=2>24</option>
+                                    <option value=4>48</option>
+                                    @break
+
+                                @case(4)
+                                    <option value=4>48</option>
+                                    <option value=0>unlimited</option>
+                                    <option value=1>12</option>
+                                    <option value=2>24</option>
+                                    <option value=3>36</option>
+                                    @break
+                            @endswitch
+                        </select>
+                    </div>
+
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
     </div>
     {!! Form::close() !!}
 
